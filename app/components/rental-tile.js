e4bf58f25f73278f5f127f3cf50c1ec42c201e7d
Courtney Phillips <courtney@epicodus.com>
2016-06-06 12:19:12 -0700

    Move delete and update actions from rental-tile component (and therefore Index route where it is rendered) into new Rental route.

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -8,11 +8,6 @@ export default Ember.Component.extend({
     },
     imageHide: function() {
      this.set('isImageShowing', false);
-   },
-    delete(rental) {
-      if (confirm('Are you sure you want to delete this rental?')) {
-        this.sendAction('destroyRental', rental);
-      }
-    }
+   }
   }
 });
