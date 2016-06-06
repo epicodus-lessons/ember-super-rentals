65bda829ddac3f2219359ead39d03dee8d5520c3
Courtney Phillips <courtney@epicodus.com>
2016-06-06 14:30:20 -0700

    Add cityFormShow and saveCity actions to new-city.js to handle displaying form to add new city, and gathering form parameters and bubbling action up to template.

--- app/components/new-city.js
+++ app/components/new-city.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewCity: false,
+  actions: {
+    cityFormShow() {
+      this.set('addNewCity', true);
+    },
+    saveCity() {
+     var params = {
+       name: this.get('name'),
+       country: this.get('country')
+     };
+     this.set('addNewCental', false);
+     this.sendAction('saveCity', params);
+   }
+  }
 });
