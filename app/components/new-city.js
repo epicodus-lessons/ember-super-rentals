fe2dc2e45a4436d071ce54edaa804513226425e9
Courtney Phillips <courtney@epicodus.com>
2016-06-06 14:32:35 -0700

    Fix typo preventing new city form from hiding after submit.

--- app/components/new-city.js
+++ app/components/new-city.js
@@ -11,7 +11,7 @@ export default Ember.Component.extend({
        name: this.get('name'),
        country: this.get('country')
      };
-     this.set('addNewCental', false);
+     this.set('addNewCity', false);
      this.sendAction('saveCity', params);
    }
   }
