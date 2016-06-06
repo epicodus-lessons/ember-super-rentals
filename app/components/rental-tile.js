3552f30b2631e5ef485ebbafec6b63b3441b2713
Courtney Phillips <courtney@epicodus.com>
2016-06-06 11:25:34 -0700

    Define imageHide action in rental-tile.js to hide image when it is clicked on.

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -5,6 +5,9 @@ export default Ember.Component.extend({
   actions: {
     imageShow: function() {
       this.set('isImageShowing', true);
-    }
+    },
+    imageHide: function() {
+     this.set('isImageShowing', false);
+   }
   }
 });
