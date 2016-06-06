13dade8644a39a80a074cef69bd45fe57c68ecfe
Courtney Phillips <courtney@epicodus.com>
2016-06-06 13:33:39 -0700

    Update model hook on Index route to contain both cities and rentals in RSVP Hash.

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-     return this.store.findAll('rental');
-   },
+    return Ember.RSVP.hash({
+      cities: this.store.findAll('city'),
+      rentals: this.store.findAll('rental')
+    });
+  },
    actions: {
     save3(params) {
       var newRental = this.store.createRecord('rental', params);
