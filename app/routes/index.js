9e7c73148bd281c001db900d086f80c05b671242
Courtney Phillips <courtney@epicodus.com>
2016-06-06 11:30:05 -0700

    Add destroyRental action to Index route handler to be triggered when delete action is passed up from rental-tile component.

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,10 @@ export default Ember.Route.extend({
   model() {
      return this.store.findAll('rental');
    },
+   actions: {
+    destroyRental(rental) {
+      rental.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
