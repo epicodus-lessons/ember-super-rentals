cfe53e4dc46390ecd1ce8817522012c9822fa4a4
Courtney Phillips <courtney@epicodus.com>
2016-06-06 11:38:26 -0700

    Define save1 action in new-rental.hjs to handle collecting form input data from new-rental.js, packaging into 'params' hash, and sending action up with params.

--- app/components/new-rental.js
+++ app/components/new-rental.js
@@ -5,6 +5,17 @@ export default Ember.Component.extend({
   actions: {
     rentalFormShow() {
       this.set('addNewRental', true);
-    }
+    },
+    save1() {
+     var params = {
+       owner: this.get('owner'),
+       city: this.get('city'),
+       type: this.get('type'),
+       image: this.get('image'),
+       bedrooms: this.get('bedrooms'),
+     };
+     this.set('addNewRental', false);
+     this.sendAction('save2', params);
+   }
   }
 });
