bb095bf1d2527633db966aeb1cbea519a04dc9d4
Courtney Phillips <courtney@epicodus.com>
2016-06-08 11:29:07 -0700

    Add fullLocation computed property to city-tile component.

--- app/components/city-tile.js
+++ app/components/city-tile.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  fullLocation: Ember.computed('city.name', 'city.country', function() {
+    return this.get('city.name') + ', ' + this.get('city.country');
+  }),
   actions: {
     destroyCity(city) {
       if (confirm('Are you sure you want to delete this city?')) {
