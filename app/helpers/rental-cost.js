92c3fb75c9b73da27c06d2dce9fcf25b6357422a
Courtney Phillips <courtney@epicodus.com>
2016-06-08 11:22:35 -0700

    Populate rental-cost helper with code to return varying number of $ signs depending on cost of particular rental.

--- app/helpers/rental-cost.js
+++ app/helpers/rental-cost.js
@@ -1,7 +1,17 @@
 import Ember from 'ember';
 
-export function rentalCost(params/*, hash*/) {
-  return params;
+export function rentalCost(params) {
+  var rentalPrice = params[0].get('cost');
+
+  if (rentalPrice >= 150){
+    return '$$$$';
+  } else if (rentalPrice >= 100){
+    return '$$$';
+  } else if (rentalPrice >= 50){
+    return '$$';
+  } else if (rentalPrice <= 49){
+    return '$';
+  }
 }
 
 export default Ember.Helper.helper(rentalCost);
